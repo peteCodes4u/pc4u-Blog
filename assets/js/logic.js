9655c35a10565267a9c823529f74ea67229aeb55
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-23 14:36:50 -0400

    first pass at show content

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// logic notes
+// Notes Framework
+// 1. Form page loads first
+// 2. user click's post blog to show blog page content and footer.
+// 2.1 blog post data is stored to localStorage.  
+// 2.2 redirected to the posts page.
+// 2.3 form validators  username, title, or content (required) 
+// 2.4 confirm rendered to complete the form.
+// 3. (part A.) header with a light mode/dark mode toggle, ("back" button hidden).
+
+const toggleButton1 = document.getElementById("toggleButton1");
+toggleButton1.addEventListener("click", function() {window.location.href= "./pc4u-blog-posts.html"});
