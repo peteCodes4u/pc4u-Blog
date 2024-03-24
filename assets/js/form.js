60064d2d2ba31142e311a8bdc85884ad275b25f8
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-24 03:02:38 -0400

    Dark mode functionality applied to both form and blog css with resepective .js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,25 @@
+// dark mode
+const themeSwitch = document.getElementById("mode-switcher");
+const container = document.querySelector('.container');
+let mode = 'light'
+
+themeSwitch.addEventListener('click', function () {
+    if(mode === 'light') { 
+        mode = 'dark'
+        container.setAttribute('class', 'dark')
+    }
+    else {
+        mode = 'light'
+        container.setAttribute('class', 'light')
+    }
+})
+
 // post blog button 
 
 document.getElementById("back-button").style.display= "none";
 
 const postBlog = document.getElementById("post-blog");
 postBlog.addEventListener("click", function() {
-    window.location.href= "./pc4u-blog-posts.html"
+    window.location.href= "./blog.html"
 })
+
