6e17b09b1b624cb49090cc144d73184c7858bfee
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-24 20:45:57 -0400

    code clean up ready for merge to main

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,7 @@
-// dark mode
+// hide back-button
+document.getElementById("back-button").style.display= "none";
+
+// dark mode toggle
 const themeSwitch = document.getElementById("mode-switcher");
 const container = document.querySelector('.container');
 let mode = 'light'
@@ -14,16 +17,39 @@ themeSwitch.addEventListener('click', function () {
     }
 })
 
-// local storage (poc) solution
-const username = document.getElementById('username');
-const blogTitle = document.getElementById('blog-title');
-const blogContent = document.getElementById('blog-content');
 
-// post blog button 
+// blog post content local storage solution 
+let postItem = {};
+
+const addNuBlogPost = (nuPost) => {
+const post = window.localStorage.getItem('post');
+
+if(post === null) {
+window.localStorage.setItem('post', JSON.stringify([nuPost]));
+} else {
+const getCurrentPost = window.localStorage.getItem('post');
+const currentPost = JSON.parse(getCurrentPost);
+
+currentPost.push(nuPost);
+
+window.localStorage.setItem('post', JSON.stringify(currentPost));
+}};
+
+// post blog button
 const postBlog = document.getElementById("post-blog");
 
-
+// post blog
 postBlog.addEventListener("click", function() {
+
+    const username = document.getElementById('username');
+    const blogTitle = document.getElementById('blog-title');
+    const blogContent = document.getElementById('blog-content');
+
+    const postItem = {
+    username: username.value,
+    title: blogTitle.value,
+    content: blogContent.value,
+    }
    
     //require user to complete form
     if(
@@ -37,15 +63,9 @@ postBlog.addEventListener("click", function() {
 
     } else {
     
-
-
+    // add post item
+    addNuBlogPost(postItem);
     // redirect to blog page after successful completion of form data entry
     window.location.href= "./blog.html"
     };
 })
-
-// back-button
-document.getElementById("back-button").style.display= "none";
-
-
-
