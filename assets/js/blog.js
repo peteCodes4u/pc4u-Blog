21089c600cd576b97b3088c3f660bcc681421ea8
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-27 16:29:34 -0400

    completed changes and improved functionality for post formatting after write post function execution

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,6 +2,8 @@
 const postData = JSON.parse(localStorage.getItem('post')) || [];
 
 const postContainer = document.getElementById("post");
+const blogContainer = document.getElementById("blogContainer");
+const mainContent = document.getElementById("main-content");
 
 // back button
 const backButton = document.getElementById("back-button");
@@ -24,37 +26,20 @@ themeSwitch.addEventListener('click', function () {
 })
 
 
-                // <article class="blog-article">
-                //     <section class="blog-title">
-                //         <h3>Blog Title 1</h3>
-                //     </section>
-                //     <section class="blog-content">
-                //         <p>Blog 1 content</p>
-                //     </section>
-                //     <section class="user">
-                //         <p>peter.appliedanalyticalsciences@gmail.com</p>
-                //     </section>
-                // </article>
-
-                // const postItem = {
-                //     username: username.value,
-                //     title: blogTitle.value,
-                //     content: blogContent.value,
-
-
-
-
 const createBlog = () => {
 
     postContainer.innerHTML = '';
 
     for (const blog of postData) {
 
-        const post = document.getElementById('post')
-        const article = document.createElement('article')
-        const sectionTitle = document.createElement('section')
-        const sectionContent = document.createElement('section')
-        const sectionUser = document.createElement('section')
+
+        const blogContainer = document.createElement('div');
+        const postUserData = document.createElement('section');
+        const post = document.createElement('section');
+        const article = document.createElement('article');
+        const sectionTitle = document.createElement('section');
+        const sectionContent = document.createElement('section');
+        const sectionUser = document.createElement('section');
         const titleH3 = document.createElement('h3');
         const blogP = document.createElement('p');
         const userP = document.createElement('p');
@@ -71,11 +56,19 @@ const createBlog = () => {
         userP.textContent = blog.username;
         sectionUser.appendChild(userP);
 
+
+        blogContainer.appendChild(postUserData);
+        postUserData.appendChild(post);
+
+        post.appendChild(article);
         article.appendChild(sectionTitle);
         article.appendChild(sectionContent);
         article.appendChild(sectionUser);
 
-        postContainer.appendChild(article);
+
+        // postContainer.appendChild(article);
+        postContainer.appendChild(article)
+
 
 
     }
