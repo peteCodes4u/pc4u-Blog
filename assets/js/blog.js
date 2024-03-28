c961afc49639a48be3e5c974be34fb9b3a2636ee
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-28 11:21:05 -0400

    updated some styling and removed unnecessary files

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -25,7 +25,7 @@ themeSwitch.addEventListener('click', function () {
     }
 })
 
-
+// create blog function > runs by for of loop on page load
 const createBlog = () => {
 
     postContainer.innerHTML = '';
@@ -33,48 +33,62 @@ const createBlog = () => {
     for (const blog of postData) {
 
 
-        const blogContainer = document.createElement('div');
-        const postUserData = document.createElement('section');
+        // creates section blog posts
         const post = document.createElement('section');
+
+        // creates individual blog articles
         const article = document.createElement('article');
+
+        // creates the title section of the blog article
         const sectionTitle = document.createElement('section');
+
+        // creates the blog content section of the blog article
         const sectionContent = document.createElement('section');
+
+        // creates the blog user section of the blog article
         const sectionUser = document.createElement('section');
+
+        // creates the blog title as H3 element
         const titleH3 = document.createElement('h3');
+
+        // creates the blog post as p element
         const blogP = document.createElement('p');
+
+        // creates the blog user as a p element
         const userP = document.createElement('p');
 
+        // creates the class to apply to the title H3 element
         sectionTitle.classList.add("blog-title");
         titleH3.textContent = blog.title;
         sectionTitle.appendChild(titleH3);
 
+        // creates the class to apply to the content p element
         sectionContent.classList.add('blog-content');
         blogP.textContent = blog.content;
         sectionContent.appendChild(blogP);
 
+        // creates the class to apply to the user p element
         sectionUser.classList.add('user');
         userP.textContent = blog.username;
         sectionUser.appendChild(userP);
 
-
-        blogContainer.appendChild(postUserData);
-        postUserData.appendChild(post);
-
+        // appends the post to the article element
         post.appendChild(article);
+
+        // appends the title section to the article element 
         article.appendChild(sectionTitle);
+
+        // appends the post content to the article
         article.appendChild(sectionContent);
+
+        // appends the post user to the article
         article.appendChild(sectionUser);
 
-
-        // postContainer.appendChild(article);
+        // appends the post data to the article
         postContainer.appendChild(article)
-
-
-
     }
-
-
 }
 
+// execute create blog function to generate blog posts from local storage JSON object 'post'
 createBlog();
 
